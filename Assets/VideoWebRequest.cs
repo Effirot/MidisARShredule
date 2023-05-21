b5f0bf08a1637c87789faf52bdf4943a5b396ac0
Effirot <maximus323i@mail.ru>
2023-05-22 00:42:02 +0500

    first commit

---
+++ Assets/VideoWebRequest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoWebRequest : MonoBehaviour
+{
+    public VideoPlayer Output;
+    
+
+    public void LoadVideo() {
+
+    }
+
+    
+}
